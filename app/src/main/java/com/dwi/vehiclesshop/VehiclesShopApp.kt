e963cd67ee0173f467a492fda8d39dde9445f885
dwi <wdwihartarto@gmail.com>
2023-11-19 08:28:13 +0700

    delete detail screen

--- app/src/main/java/com/dwi/vehiclesshop/VehiclesShopApp.kt
+++ app/src/main/java/com/dwi/vehiclesshop/VehiclesShopApp.kt
@@ -1,22 +1,9 @@
 package com.dwi.vehiclesshop
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHostController
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import com.dwi.vehiclesshop.ui.component.TopBar
-import com.dwi.vehiclesshop.ui.navigation.Screen
-import com.dwi.vehiclesshop.ui.screens.detail.DetailItemScreen
 import com.dwi.vehiclesshop.ui.screens.home.HomeScreen
 
 @OptIn(ExperimentalFoundationApi::class)
